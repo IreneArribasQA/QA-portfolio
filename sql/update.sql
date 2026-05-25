466a928d39e9f7394394992e619f2ad7998a3e45
Irene Arribas Méndez <irenearribasmendez@gmail.com>
2026-05-25 15:22:31 +0200

    Update update.sql
    dejo solo los update y   borro los delete

--- sql/update.sql
+++ sql/update.sql
@@ -7,9 +7,3 @@ WHERE name = 'María';
 UPDATE formirenearribas
 SET age = 21
 WHERE age =20;
-
---Eliminar todos los registros con mensaje que contenga test
-DELETE FROM formirenearribas WHERE message LIKE '%test%';
-
---Eliminar todos los registros con email pedro@gmail.com
-DELETE FROM formirenearribas WHERE email = 'pedro@gmail.com';
